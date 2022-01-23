4fdad5d42ad2c5692892b7a847f93d6cb0abbb97
Danny Shen <shenfive@gmail.com>
2022-01-23 10:10:29 +0800

    new page

---
+++ A202HelloLife/NewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewViewController.swift
+//  A202HelloLife
+//
+//  Created by 申潤五 on 2022/1/23.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("New Page")
+    }
+    
+
+}
