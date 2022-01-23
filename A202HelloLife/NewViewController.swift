6a8bb629a6835d392177f4785e26b442de0a1f90
Danny Shen <shenfive@gmail.com>
2022-01-23 10:16:43 +0800

    view did app

--- A202HelloLife/NewViewController.swift
+++ A202HelloLife/NewViewController.swift
@@ -13,7 +13,23 @@ class NewViewController: UIViewController {
         super.viewDidLoad()
 
         print("New Page")
+        
+        let view1 = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        view1.backgroundColor = UIColor.red
+        self.view.addSubview(view1)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let view2 = UIView(frame: CGRect(x: 100, y: 250, width: 100, height: 100))
+        view2.backgroundColor = UIColor.blue
+        self.view.addSubview(view2)
+        
+        
+    }
+    
+    
     
 
 }
